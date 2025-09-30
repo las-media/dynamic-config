0368f9ff58477278993439f682b2b9af3648c3e0
Michał Pawluczuk <michal.pawluczuk@lasmedia.me>
2025-09-30 10:53:56 +0200

    Update ConfigSources type import

--- lib/index.ts
+++ lib/index.ts
@@ -2,6 +2,7 @@ export { getConfig, isConfigLoaded } from './browser/config';
 export { defineConfigEntry } from './browser/define-config-entry';
 export { initConfig } from './browser/init-config';
 export type {
+  ConfigSources,
   GenerateConfigFunction,
   InitConfigOptions,
 } from './browser/types';
